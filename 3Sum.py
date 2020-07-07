83ad362626d0df6f6137cdfc4cfe733417669bef
unknown <zxli@ncree.org>
2020-07-07 09:51:48 +0800

    3Sum upload

---
+++ 3Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+
+
+
+
+
+if __name__ == "__main__":
+    a = Solution()
+    prevMap = {}
+        
+
--- TwoSum.py
+++ TwoSum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target) :
         prevMap = {}
         
         for i, n in enumerate(nums):
