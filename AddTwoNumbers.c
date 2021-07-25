ea29ecee9f279d629424ae5cdf619c75e16ea4f5
oldman3483 <qazws3483@gmail.com>
2021-07-25 17:22:33 +0800

    add main function testing code

--- AddTwoNumbers.c
+++ AddTwoNumbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct ListNode{
     int val;
@@ -34,5 +35,34 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
 
 int main(void)
 {
+    struct ListNode a1, b1, c1;
+    struct ListNode a2, b2, c2; 
+    struct ListNode *l1 = &a1; 
+    struct ListNode *l2 = &a2;
+
+
+    // l1=582 l2=123
+    a1.val = 5;
+    a1.next = &b1;
+    b1.val = 6;
+    b1.next = &c1;
+    c1.val = 2;
+    c1.next = NULL;
+
+    a2.val = 3;
+    a2.next = &b2;
+    b2.val = 2;
+    b2.next = &c2;
+    c2.val = 1;
+    c2.next = NULL;
+
+    struct ListNode *ans = addTwoNumbers(l1, l2);
+    while(ans!=NULL){
+        printf("address=%p, ",ans); 
+        printf("data=%d ",ans->val);  
+        printf("next=%p\n",ans->next); 
+        ans=ans->next;  
+    } 
+
     return 0;
 }
