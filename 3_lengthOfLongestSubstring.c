e0578fb5de94f27dd80b3b10ea4bdb26e1f008a0
oldman <qazws3483@gmail.com>
2022-01-21 00:15:32 +0800

    can run, logic fault

--- 3_lengthOfLongestSubstring.c
+++ 3_lengthOfLongestSubstring.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int lengthOfLongestSubstring(char * s){
@@ -7,30 +8,41 @@ int lengthOfLongestSubstring(char * s){
     char *l_ptr;
     int cnt=0;
     int max=0;
+    int stone=0;
     r_ptr = s;
     l_ptr = s;
-    /*
+    
     for(int i=0;i<strlen(s);i++)
     {
-
+        l_ptr = s + stone;
         for(int j=0;j<i;j++)
         {
-
+            if (*r_ptr == *l_ptr)
+                stone = j;
+                cnt = 0;
+                r_ptr = l_ptr;
+            l_ptr++;
         }
-        
+        if (cnt>max)
+            max = cnt;
+        r_ptr++;
+        cnt++;
+        max++;
     }
-    */
+    
+    
    
-   printf("%d", s);
+   printf("\n%d", s);
    printf("\n");
-   printf("%s", r_ptr+1);
-   return 0;
+   printf("%d", r_ptr+1);
+   return max;
 }
 
 int main(void)
 {
-
+    int out;
     char *s = "abcabcbb";
     printf("%s", s);
-    lengthOfLongestSubstring(s);
+    out = lengthOfLongestSubstring(s);
+    printf("\n%d", out);
 }
