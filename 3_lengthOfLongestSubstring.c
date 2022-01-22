144572145121d03b57cf2cae8acbfbba19e9c51d
oldman <qazws3483@gmail.com>
2022-01-22 22:48:55 +0800

    change alg with the web solution

--- 3_lengthOfLongestSubstring.c
+++ 3_lengthOfLongestSubstring.c
@@ -4,44 +4,27 @@
 
 
 int lengthOfLongestSubstring(char * s){
-    char *r_ptr;
-    char *l_ptr;
-    int cnt=1;
-    int max=0;
-    int stone=0;
-    r_ptr = s;
-    l_ptr = s;
+    int *string_index = malloc(sizeof(int)*1024);
+    int start_index=0;
+    int max = 0;
+    int cnt = 0;
+
+    for (int i=0; i<1024; i++)
+        string_index[i] = -1;
     
     for(int i=0;i<strlen(s);i++)
     {
-        l_ptr = s + stone;
-        for(int j=0;j<i;j++)
+        if (string_index[s[i]] > start_index)
         {
-            printf("\n%c, %c", r_ptr[i], l_ptr[j]);
-
-            if (r_ptr[i] == l_ptr[j])
-                stone = j;
-                cnt = 0;
-                r_ptr = l_ptr;
-                break;
-            l_ptr++;
-            printf("\nl_ptr pos is %c", l_ptr[j]);
+            if (cnt>max)
+                max = string_index[s[i]] - start_index+1;
+            start_index = string_index[s[i]];
+            cnt = 0;
         }
-        if (cnt>max)
-            max = cnt;
-        r_ptr++;
+        else
+            string_index[s[i]] = i;
         cnt++;
-        printf("\n%d", cnt);
-        printf("\n!!!!!r_ptr pos is %c", r_ptr[i]);
-
-
     }
-    
-    
-   
-   //printf("\n%d", s);
-   //printf("\n");
-   //printf("%d", r_ptr+1);
    return max;
 }
 
