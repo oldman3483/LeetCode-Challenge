44389b8a8d93d976b38be1533da51f30a7faa4d6
oldman <qazws3483@gmail.com>
2022-01-23 00:51:20 +0800

    complete q3

--- 3_lengthOfLongestSubstring.c
+++ 3_lengthOfLongestSubstring.c
@@ -7,23 +7,28 @@ int lengthOfLongestSubstring(char * s){
     int *string_index = malloc(sizeof(int)*1024);
     int start_index=0;
     int max = 0;
-    int cnt = 0;
-
     for (int i=0; i<1024; i++)
         string_index[i] = -1;
     
     for(int i=0;i<strlen(s);i++)
     {
-        if (string_index[s[i]] > start_index)
+        if (string_index[s[i]] >= start_index)
         {
-            if (cnt>max)
-                max = string_index[s[i]] - start_index+1;
-            start_index = string_index[s[i]];
-            cnt = 0;
+            start_index = string_index[s[i]]+1;
+            string_index[s[i]] = i; 
+
+            printf("start i = %d", start_index);
+            printf("\n");
         }
         else
+        {
             string_index[s[i]] = i;
-        cnt++;
+                if (i-start_index+1>max)
+                    max = string_index[s[i]] - start_index +1;
+        }
+        printf("\ni = %d", i);
+        printf("\n");
+        
     }
    return max;
 }
@@ -32,7 +37,8 @@ int main(void)
 {
     int out;
     char *s = "abcabcbb";
+    char *s2 = "aab";
     printf("%s", s);
-    out = lengthOfLongestSubstring(s);
+    out = lengthOfLongestSubstring(s2);
     printf("\n%d", out);
 }
