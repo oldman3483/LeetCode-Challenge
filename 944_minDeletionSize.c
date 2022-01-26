7330de4ffd97ade6a72fb8b018f31f81708efd6d
oldman <qazws3483@gmail.com>
2022-01-27 00:44:25 +0800

    finish row col practice

--- 944_minDeletionSize.c
+++ 944_minDeletionSize.c
@@ -4,5 +4,35 @@
 
 
 int minDeletionSize(char ** strs, int strsSize){
+    // *(*(A+行)+列)
+
+    int j = 0;
+    int del = 0;
+
+    while (*(*(strs)+j) != 0 )
+    {
+        for (int i=1; i<strsSize; i++)
+        {
+            if(*(*(strs+i)+j) < *(*(strs+i-1)+j))
+            {
+                del++;
+                break;
+            }
+        }
+        j++;
+    }
+    return del;
+}
+
+
+
+
+
+int main(void)
+{
+    char *strs[] = {"cba","daf","ghi"};
+    int out ;
+    out = minDeletionSize(strs, 3);
+    printf("%d\n", out);
 
 }
