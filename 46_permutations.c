abc562fec0ffd1da7b3fd561a1a5b67fc2f40d38
oldman <qazws3483@gmail.com>
2022-02-06 05:37:47 +0800

    soooo hard

--- 46_permutations.c
+++ 46_permutations.c
@@ -1,79 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void __swap(int* nums, int n, int m)
+# define LEN 0xffff
+void swap(int* a, int* b)
 {
-    int tmp = nums[n];
-    nums[n] = nums[m];
-    nums[m] = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-void nextPermute(int* nums, int numsSize)
+void recv(int *arr, int len, int index, int **ret, int* ret_index)
 {
-    if (numsSize <= 1) {
+    int i;
+    if(index==len-1)
+    {
+        ret[*ret_index] = (int*)malloc(sizeof(int)*len);
+        memcpy(ret[*ret_index], arr, len * sizeof(int));
+        (*ret_index)++;
         return;
     }
-    if (numsSize == 2) {
-        __swap(nums, 0, 1);
-        return;
-    }
-    /* // one pointer will cause abort error waste to much time, out of the runtime
-    for(int end=numsSize-1; end>0; end--)
+    for (i=index; i<len; i++)
     {
-        if (nums[end]>nums[end-1])
-        {
-            des_first = end;
-            break;
-        }
-    } 
-    */
-    // two pointer 
-    int des_first = numsSize;
-    for(int end=numsSize-1, end_next=end-1 ; end>=0; end--, end_next--)
-    {
-        if (end_next<0 || nums[end]>nums[end_next])
-        {
-            des_first = end;
-            break;
-        }
-    }
-    int higher = des_first;
-
-    for(int i = des_first; i<numsSize; i++)
-    {
-        if(nums[i]>nums[des_first-1])
-        {
-            higher = i;
-        }
-    }
-    __swap(nums, des_first-1, higher);
-    
-    int start = des_first;
-    int end_des = numsSize-1;
-    while(start < end_des)
-    {
-        __swap(nums, start, end_des);
-        start++;
-        end_des--;
+        swap(&arr[i], &arr[index]);
+        printf("ret = %d\n", ret[i]);
+        recv(arr, len, index+1, ret, ret_index);
+        swap(&arr[i], &arr[index]);
     }
 }
+
 
 int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+    int** ans = (int**)malloc(sizeof(int*)*LEN);
+    int* retSize = (int*)malloc(sizeof(int)*LEN);
+    int retIndex =0;
+    int i =0;
 
-    return 0;
+    recv(nums, numsSize, 0, ans, &retIndex);
+    *returnColumnSizes=retSize;
+    for(i=0; i<retIndex; i++)
+    {
+        retSize[i] = numsSize;
+    }
+    *returnSize = retIndex;
+    return ans;
 }
 
 int main(void)
 {
-    int nums[3] = {3, 2, 1};
-    int size_nums = 3;
-    nextPermute(nums, size_nums);
-    
-    for(int i=0; i<size_nums;i++)
-    {
-        printf("%d", *(nums+i));
-
-    }
-    
     return 0;
 }
