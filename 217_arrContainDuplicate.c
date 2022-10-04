2b46f949d44080ad752aa7e99ce8caa090ccb481
oldman <qazws3483@gmail.com>
2022-10-04 22:48:52 +0800

    modify 217 code

--- 217_arrContainDuplicate.c
+++ 217_arrContainDuplicate.c
@@ -15,32 +15,6 @@ bool containsDuplicate(int* nums, int numsSize){
 
 
 // SORT O(nlogn)
-
-void swap(int* a, int* b)
-{
-    int tmp = *a;
-    *a = *b;
-    *b = tmp; 
-}
-
-void qsort(int* nums, int left, int right)
-{
-    int i = left-1;
-    int j = right+1;
-    int pIdx = 1/2*(left+right);
-
-    while(1)
-    {
-        while(nums[++i] < nums[pIdx]);
-        while(nums[++j] > nums[pIdx]);
-        if (i >= j)
-            break;
-        swap(nums[i], nums[j]);
-    }
-    qsort(nums, left, pIdx-1);
-    qsort(nums, pIdx+1, right);
-
-}
 void swap(int* a, int* b)
 {
     int tmp = *a;
@@ -49,29 +23,32 @@ void swap(int* a, int* b)
 }
 #define SWAP(x,y) {int t; t = x; x = y; y = t;} 
 
+void Qsort(int* nums, int left, int right)
+{
+    if (right > left)
+    {
+        int i = left-1;
+        int j = right+1;
+        int s = nums[(left+right)/2];
 
-void quickSort(int number[], int left, int right) { 
-    if(left < right) { 
-        int s = number[(left+right)/2]; 
-        int i = left - 1; 
-        int j = right + 1; 
+        while(1)
+        {
+            while(nums[++i] < s);
+            while(nums[--j] > s);
+            if (i >= j)
+                break;
+            swap(&nums[i], &nums[j]);
+        }
+        Qsort(nums, left, i-1);
+        Qsort(nums, j+1, right);
+    }
 
-        while(1) { 
-            while(number[++i] < s) ;  // 向右找 
-            while(number[--j] > s) ;  // 向左找 
-            if(i >= j) 
-                break; 
-            SWAP(number[i], number[j]); 
-        } 
+}
 
-        quickSort(number, left, i-1);   // 對左邊進行遞迴 
-        quickSort(number, j+1, right);  // 對右邊進行遞迴 
-    } 
-} 
 bool containsDuplicate(int* nums, int numsSize){
 
     int i = 0;
-    quickSort(nums, 0, numsSize-1);
+    Qsort(nums, 0, numsSize-1);
     while(i+1 < numsSize)
     {
         if(nums[i] == nums[i+1])
@@ -80,6 +57,7 @@ bool containsDuplicate(int* nums, int numsSize){
     }
     return false;
 }
+
 
 // HASH O(n)
 
