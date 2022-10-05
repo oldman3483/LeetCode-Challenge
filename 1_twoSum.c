775c46c3873c99c121beefcde64b719423383f0c
oldman <qazws3483@gmail.com>
2022-10-06 01:34:04 +0800

    hash and brutal fixed with new format twoSum

--- 1_twoSum.c
+++ 1_twoSum.c
@@ -1,16 +1,17 @@
 // brutal 
 
-int* twoSum(int nums[], int numsSize, int target) {
-
+int* twoSum(int nums[], int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
     int result;
-    int *index = malloc(sizeof(int)*2);
+    int *index = (int*)malloc(sizeof(int)*2);
     
     for(int i=0; i<numsSize; i++)
     {
+        result = target - nums[i];
         for(int j=i+1; j<numsSize; j++)
         {
-            result = nums[i]+nums[j];
-            if(result == target)
+            //result = nums[i]+nums[j];
+            if(result == nums[j])
             {
                 index[0] = i;
                 index[1] = j;
@@ -20,7 +21,7 @@ int* twoSum(int nums[], int numsSize, int target) {
         }
     }
     
-    return NULL;
+    return index;
 }
 
 
